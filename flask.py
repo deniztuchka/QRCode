4d607acbdcd23845152afb6cf9cd47a0fa7d5b0c
deniztuchka <162617507+deniztuchka@users.noreply.github.com>
2025-03-10 13:06:40 +0100

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request, send_file
 import qrcode
 from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    qr_code = None
     if request.method == 'POST':
         text = request.form['text']
         qr = qrcode.make(text)
+
         img_io = BytesIO()
         qr.save(img_io, 'PNG')
         img_io.seek(0)
-        return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='qrcode.png')
-    return render_template('index.html')
+
+        # QR kodunu base64 formatına çevirerek HTML'de göstermek için
+        qr_code = base64.b64encode(img_io.getvalue()).decode()
+
+    return render_template('index.html', qr_code=qr_code)
 
 if __name__ == '__main__':
     app.run(debug=True)
